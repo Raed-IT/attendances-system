ca6ef0aa64937d116d8d85daff4e6650c29e2e05
Raed-IT <RaedSwanIT@gmail.com>
2023-06-23 22:44:39 +0300

    filter attends

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use Spatie\Period\Precision;
 
 Route::get('/', function () {
     //2023-06-01 00:00:00
-    dd(Carbon::parse("2023-06-01 00:00:00"));
+//    dd(Carbon::parse("2023-06-01 00:00:00"));
     //    $records = Attendance::where([
 //        ["timestamp", '>', now()->firstOfMonth()->format('Y-m-d')],
 //        "user_id" => 1190
